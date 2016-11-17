bd4addeb0f9ff855a4ce4710ad952e3c5875e66c
Julian Neuhaus <neuhaus.julian@gmail.com>
2016-11-17 13:08:03 +0100

    added testable class
    added first class which can be tested

---
+++ src/main/java/de/jn/test/Test.java
@@ -0,0 +1,16 @@
+/**
+  SOME GPLv3-License-Text
+**/
+package de.jn.test;
+
+/**
+ * @author Julian Neuhaus <neuhaus.julian@gmail.com>
+ * @sine 1.0.0
+ */
+public class Test
+{
+	public int add(int intOne, int intTwo)
+	{
+		return intOne + intTwo;
+	}
+}
