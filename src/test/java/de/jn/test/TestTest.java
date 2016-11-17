2f1e6713dc2db60511036a0ab06c13f51a9b3bd1
Julian Neuhaus <neuhaus.julian@gmail.com>
2016-11-17 13:21:03 +0100

    update Tests
    added first Test-Case

---
+++ src/test/java/de/jn/test/TestTest.java
@@ -0,0 +1,27 @@
+/**
+  SOME GPLv3-License-Text
+**/
+package de.jn.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author Julian Neuhaus <neuhaus.julian@gmail.com>
+ * @sine 1.0.0
+ */
+public class TestTest
+{
+	@Test
+	public void addTest()
+	{
+		final int valueOne = 3;
+		final int valueTwo = 4;
+		final int validResult = 7;
+		
+		de.jn.test.Test testInstance = new de.jn.test.Test();
+		
+		assertTrue(testInstance.add(valueOne, valueTwo) == validResult);
+	}
+}
